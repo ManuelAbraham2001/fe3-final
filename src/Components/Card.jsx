74c3018804de23ec0cc925fa1fb91a6697b47fcd
manu <manu.20012009@gmail.com>
2023-09-15 19:45:29 -0300

    fix card link

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { useGlobalContext } from "./utils/global.context";
+import { Link } from "react-router-dom";
 
 
 const Card = ({ name, username, id }) => {
@@ -18,12 +19,12 @@ const Card = ({ name, username, id }) => {
 
   return (
     <div className="card">
-      <a href={`/dentist/${id}`}>
+      <Link to={`/dentist/${id}`}>
         <span>ID: {id}</span>
         <h2>Nombre: {name}</h2>
         <h3>Usuario: {username}</h3>
         
-      </a>
+      </Link>
       <button style={isFavorite ? {color: "red"} : null} onClick={toggleFavorite} className={`favButton ${isFavorite ? 'favActive' : ''}`}>
         {isFavorite ? 'Borrar de favoritos' : 'Agregar a favoritos'}
       </button>
